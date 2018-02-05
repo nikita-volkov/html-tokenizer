ad17c5ad80a42cc3654aa750129d3fad29757ef6
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-02-05 18:09:09 +0300

    Add Generic instances

--- library/HTMLTokenizer/Data.hs
+++ library/HTMLTokenizer/Data.hs
@@ -3,9 +3,11 @@ module HTMLTokenizer.Data
   module HTMLTokenizer.Data.Types,
   module HTMLTokenizer.Data.Instances.Show,
   module HTMLTokenizer.Data.Instances.Eq,
+  module HTMLTokenizer.Data.Instances.Generic,
 )
 where
 
 import HTMLTokenizer.Data.Types
 import HTMLTokenizer.Data.Instances.Show
 import HTMLTokenizer.Data.Instances.Eq
+import HTMLTokenizer.Data.Instances.Generic
---
+++ library/HTMLTokenizer/Data/Instances/Generic.hs
@@ -0,0 +1,10 @@
+module HTMLTokenizer.Data.Instances.Generic
+where
+
+import HTMLTokenizer.Prelude
+import HTMLTokenizer.Data.Types
+
+
+deriving instance Generic Token
+deriving instance Generic Name
+deriving instance Generic Attribute
